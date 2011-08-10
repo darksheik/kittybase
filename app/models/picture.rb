3afd9c2f7f90f9bcbcefe828db3e06952e3b476d
Don Pflaster <dpflaster@gmail.com>
2011-08-09 22:12:08 -0400

    Added navigation links to individual pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,4 +5,14 @@ has_attached_file :avatar,
                                 :thumb => "100x100>" }
 
 scope :profile_pic, where(:is_profile_pic => "t")
+
+def previous_pic
+  self.class.first(:conditions => ["id < ? and kitty_id = ?", id, kitty_id], :order => "id desc")
+end
+
+def next_pic
+  self.class.first(:conditions => ["id > ? and kitty_id = ?", id, kitty_id], :order => "id asc")
+end
+
+
 end
