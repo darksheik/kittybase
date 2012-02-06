162aea898f059bc4e7d3fe07773fc3d2e332b52a
Don Pflaster <dpflaster@gmail.com>
2012-02-06 09:49:47 -0500

    Changed deployment target

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,103 +1,49 @@
-set :application, 'kittybase'
-set :repository, "git@github.com:darksheik/kittybase.git"
-set :deploy_to, "/home/dpflaster/rails/#{application}"
-set :rake, 'rake'
-
-# This change was done enable the ability to deploy a branch to
-# staging using the parameters on the command line.
-# to deploy mobile, $> cap -S branchto=mobile staging deploy
-# to deploy master by default, $> cap staging deploy
-begin
-  set :branchto, (branchto||'master')
-rescue
-  set :branchto, 'master'
-end
-
-# This change was done to be able to deploy to only one production server
-begin
-  set :serverto, (serverto||'all')
-rescue
-  set :serverto, 'all'
-end
-
-task :production do
-  if ( "#{serverto}" == "all")
-    role :web, "#{application}.sameerchowdhury.com"
-    role :app, "#{application}.sameerchowdhury.com"
-    role :db, "#{application}.sameerchowdhury.com", :primary => true
-  else
-    role :web, "#{serverto}"
-    role :app, "#{serverto}"
-    role :db, "#{serverto}", :primary => true
-  end
-
-  #set :ssh_options, {:forward_agent => true, :paranoid => false}
-  set :deploy_role, 'production'
-  set :rails_env, 'production'
-  set :branch, "#{branchto}"
-end
-
-set :keep_releases, 5
-
-set :user, 'dpflaster'
-set :use_sudo, false
+set :application, "kittybase"
+set :repository,  "git@github.com:darksheik/kittybase.git"
 
 set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-set :deploy_via, :remote_cache
+role :web, "donpflaster.com"                          # Your HTTP server, Apache/etc
+role :app, "donpflaster.com"                          # This may be the same as your `Web` server
+role :db,  "donpflaster.com", :primary => true # This is where Rails migrations will run
 
-#set :rake, '/usr/local/bin/rake'
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
 
-# This will execute the Git revision parsing on the *remote* server rather than locally
-set :real_revision, lambda { source.query_revision(revision) { |cmd| capture(cmd) } }
+set :deploy_to, "/home/donpflas/#{application}"
+set :user, "donpflas"
+set :use_sudo, false
 
-after "deploy:symlink", "deploy:symlink_configs"
-after "deploy:symlink_configs", "deploy:symlink_bundle"
-
-
-namespace :deploy do
-  task :migrate do
-    run("source /etc/profile; cd #{current_path}; rake RAILS_ENV=#{rails_env} db:migrate")
-  end
-  
-  task :symlink_bundle, :roles => :app, :except => {:no_symlink => true} do
-    run <<-CMD
-      cd #{release_path} && ln -nfs #{shared_path}/vendor/bundle #{release_path}/vendor/bundle
-    CMD
-    run "source /etc/profile; cd #{release_path} && bundle install --gemfile #{release_path}/Gemfile --path #{shared_path}/bundle --deployment --quiet --without development test "
-  end
-
-  task :symlink_configs, :roles => :app, :except => {:no_symlink => true} do
-    run <<-CMD
-      cd #{release_path} && ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml
-    CMD
-  end
-  
-  task :start, :roles => :app do
-    run "source /etc/profile; source ~/.bash_profile; cd #{current_path}; passenger start -p 9001 -d -e #{rails_env}"
-    #run "touch #{current_release}/tmp/restart.txt"
-  end
-
-  task :stop, :roles => :app do
-    run "source /etc/profile; source ~/.bash_profile; cd #{current_path}; passenger stop -p 9001;"
-  end
-
-  task :restart, :roles => :app do
-    #run "touch #{current_release}/tmp/restart.txt"
-    stop
-    start
-  end
-
+after "deploy", "deploy:bundle_gems"
+#after "deploy", "deploy:bundle_gems"
+after "deploy:bundle_gems", :roles => [:web, :db, :app] do
+  run "chmod 755 #{release_path}/public -R" 
 end
+after "deploy:bundle_gems", "deploy:precompile"
+after "deploy:precompile", "deploy:dbmigrate"
+after "deploy:dbmigrate", "deploy:copyht"
+after "deploy:copyht", "deploy:restart"
 
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    run("cd #{release_path}/vendor; ln -s #{shared_path}/vendor/bundle bundle")
+
+# If you are using Passenger mod_rails uncomment this:
+ namespace :deploy do
+   task :bundle_gems do
+     run "cd #{deploy_to}/current && bundle install vendor/gems"
+   end
+   task :copyht do
+     run "cd /home/donpflas/ && cp htac-passenger-kb public_html/kittybase/.htaccess"
+   end
+   task :dbmigrate do
+     run "cd #{deploy_to}/current && rake db:migrate RAILS_ENV=#{rails_env}"
+   end
+  task :precompile, :roles => :app do
+    run "cd #{release_path} && rake RAILS_ENV=#{rails_env} assets:precompile"
   end
- 
-  task :bundle_new_release, :roles => :app do
-    run "source /etc/profile; source ~/.bash_profile; cd #{release_path} && bundle install --gemfile #{release_path}/Gemfile --path #{shared_path}/bundle --deployment --quiet --without development test "
-  end
-end
- 
-after 'deploy:update_code', 'bundler:bundle_new_release'
+
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
